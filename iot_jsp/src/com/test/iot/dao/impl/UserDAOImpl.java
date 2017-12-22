3c4a3184e3667cdaad07804b971e6f4f5eb28689
imhyejin <imagine.codes@gmail.com>
2017-12-22 18:14:43 +0900

    asdf

--- iot_jsp/src/com/test/iot/dao/impl/UserDAOImpl.java
+++ iot_jsp/src/com/test/iot/dao/impl/UserDAOImpl.java
@@ -1,16 +1,41 @@
 package com.test.iot.dao.impl;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import com.test.iot.common.DBCon;
 import com.test.iot.dao.UserDAO;
 
 public class UserDAOImpl implements UserDAO{
 
 	@Override
 	public ArrayList<HashMap<String, Object>> selectUserList() {
-		// TODO Auto-generated method stub
-		return null;
+		PreparedStatement ps = null;
+		ResultSet rs = null;
+		ArrayList<HashMap<String, Object>> userList = new ArrayList<HashMap<String, Object>>();
+		String sql = "select * from user_info";
+		try {
+			ps = DBCon.getCon().prepareStatement(sql);
+			rs = ps.executeQuery();
+			while(rs.next()) { //밑으로 내리고 있는지 없는지 확인 후 return
+				HashMap<String, Object> hm = new HashMap<String, Object>();
+				hm.put("uino", rs.getString("uino"));
+				hm.put("uiname", rs.getString("uiname"));
+				hm.put("uiage", rs.getString("uiage"));
+				hm.put("uiid", rs.getString("uiid"));
+				hm.put("uipwd", rs.getString("uipwd"));
+				hm.put("cino", rs.getString("cino"));
+				hm.put("uiregdate", rs.getString("uiregdate"));
+				hm.put("address", rs.getString("address"));
+				userList.add(hm);
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return userList;
 	}
 
 	@Override
